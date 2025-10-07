c4959f04eee6a5bc8a220fd65cdca290a0f42563
Ahmed Yousef <AYousef@steelnetwork.com>
2025-10-07 04:05:17 +0300

    Add CustomerRepository tests and in-memory DB for testing
    Introduced `CustomerRepositoryTests` to validate repository
    methods such as `GetAllAsync`, `GetByIdAsync`, `ExistsByEmailAsync`,
    `ExistsAsync`, `AddAsync`, `UpdateAsync`, and `DeleteAsync`.
    
    Added `InMemoryDbContext` for isolated in-memory database testing.
    Updated `LayeredArch.Tests.csproj` to include the
    `Microsoft.EntityFrameworkCore.InMemory` package and a project
    reference to `LayeredArch.Infrastructure`.
    
    Test cases cover scenarios for adding, updating, deleting, and
    retrieving customers, ensuring repository correctness and handling
    of edge cases.

---
+++ LayeredArch.Tests/InMemoryDbContext.cs
@@ -0,0 +1,19 @@
+﻿using LayeredArch.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace LayeredArch.Tests;
+
+internal class InMemoryDbContext : ApplicationDbContext
+{
+
+
+    public InMemoryDbContext() : base(new DbContextOptionsBuilder<ApplicationDbContext>()
+        .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options)
+    { }
+
+    public override void Dispose()
+    {
+        Database.EnsureDeleted();
+    }
+}
